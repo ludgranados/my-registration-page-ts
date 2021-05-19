9de327c7eaabf282161108e86f143f25ed2934fb
Ludwin Granados <ludwingranados@gmail.com>
2021-05-19 16:01:28 -0700

    Feature:API

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -24,7 +24,7 @@
 
 const HomeComponent = () => {
     return (
-      <div className='Home'>
+      <div className='Home text-center'>
        <div>This is my Home Page!!</div>
       </div>
     );
